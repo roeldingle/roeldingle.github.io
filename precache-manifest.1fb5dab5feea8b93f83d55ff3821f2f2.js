b409a88babae68128c5c71128acccd0fb41232ad
Roel Dingle <rdingle@SAC-LPT-0129.straightarrow.com>
2019-09-13 23:50:18 +0800

    Updates

---
+++ precache-manifest.1fb5dab5feea8b93f83d55ff3821f2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ce909953ff624dcd0809e0751a62dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343ade74cf9470c0144a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2cc8a9b70acebea7a750",
+    "url": "/static/js/2.fadd7498.chunk.js"
+  },
+  {
+    "revision": "343ade74cf9470c0144a",
+    "url": "/static/js/main.2f220395.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
