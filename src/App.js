4028a96c9d88043683e3d947b63f50fee9fb54cb
Roel Dingle <rdingle@SAC-LPT-0129.straightarrow.com>
2019-09-15 19:40:04 +0800

    setup menu, animation, social

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 import './Animate.css';
-import { AwesomeButton, AwesomeButtonSocial } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
+import { AwesomeButton, AwesomeButtonSocial } from "react-awesome-button";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import MenuComponent from './components/MenuComponent.js';
 
 function App() {
 
   return (
     <div className="App">
       <Container maxWidth="sm">
+        <MenuComponent />
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} className="column-left">
             <h1 className="title animated rubberBand">Roel Dingle</h1>
             <h3 className="subtitle animated fadeInUp delay-1s fast">Web Development</h3>
-            <div className="social-wrap">
-              <AwesomeButtonSocial type="linkedin" url="https://www.linkedin.com/in/roeldingle/"></AwesomeButtonSocial>&nbsp;
+            <div className="social-wrap animated zoomIn delay-2s fast">
+              <AwesomeButtonSocial type="linkedin" href="https://www.linkedin.com/in/roeldingle/" target="__blank"></AwesomeButtonSocial>&nbsp;
               <AwesomeButtonSocial type="messenger" url="http://m.me/roel.dingle/"></AwesomeButtonSocial>&nbsp;
               <AwesomeButtonSocial type="github" href="https://github.com/roeldingle" target="__blank"></AwesomeButtonSocial>
             </div>
